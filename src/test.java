af96c55dca013ef9e9cf56857b222ed3d464bd68
Andy Chiang <andy10801@gmail.com>
2021-03-11 00:46:50 +0800

    move QuickUnion to example folder

--- src/test.java
+++ src/test.java
@@ -1,14 +1,5 @@
 public class test {
 	public static void main(String[] args) {
-		QuickUnion demo = new QuickUnion(8);
-		
-		demo.union(4, 3);
-		demo.union(3, 7);
-		demo.union(6, 5);
-		demo.union(3, 1);
-		
-		demo.connected(3, 7);
-		
-		demo.show();
+		// todo
 	}
 }
