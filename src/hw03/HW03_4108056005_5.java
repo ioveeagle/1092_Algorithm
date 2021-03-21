98f6fa80af70b2adc2477af003713ec9e9c07300
Andy Chiang <andy10801@gmail.com>
2021-03-22 00:56:33 +0800

    hw03: 5_kashiwa

---
+++ src/hw03/HW03_4108056005_5.java
@@ -0,0 +1,29 @@
+// case5: kashiwa
+//package hw03;
+
+public class HW03_4108056005_5 extends HillFinding{
+	public static void main(String[] args) 
+	{
+//		System.out.println("case5:");
+//		HW03_4108056005_5 test = new HW03_4108056005_5();
+//		int[] A = new TestDataGenerator().readData();
+//		Stopwatch stopwatch = new Stopwatch();
+//		System.out.println("hill is: "+test.H_Finding(A));
+//		double time = stopwatch.elapsedTime();
+//		System.out.println("elapsed time " + time);
+	}
+	
+	public int H_Finding(int[] A){
+        int start = 0;
+        int end   = A.length - 1;
+        if(A[start] < A[end]) return -1;
+        for(int mid = (start+end) >> 1; mid!=start; mid = (start+end) >> 1){
+            if(A[mid] > A[start]){
+                start = mid;
+            }else{
+                end = mid;
+            }
+        }
+        return A.length - end - 1;
+    }
+}
