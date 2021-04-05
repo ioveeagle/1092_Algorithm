f2d82d80a75cdb9bf14d5aec5c405f5485f1b526
Andy Chiang <andy10801@gmail.com>
2021-04-05 15:22:41 +0800

    hw05: new 2&3&4

--- src/hw05/HW05_4108056005_2.java
+++ src/hw05/HW05_4108056005_2.java
@@ -1,8 +1,8 @@
-// case2_hashmap + slope + thread: O(1/2*N^2)
+// case2_hashmap + deltaX & deltaY + thread32: O(1/2*N^2)
 //package hw05;
 
-public class HW05_4108056005_2 extends LLK {
-	
+public class HW05_4108056005_2 extends LLK 
+{
 	public static void main(String[] args) 
 	{
 //		HW05_4108056005_2 test = new HW05_4108056005_2();
@@ -14,102 +14,213 @@ public class HW05_4108056005_2 extends LLK {
 //		System.out.println("elapsed time " + time);
 	}
 	
+	int[][] array;
 	volatile boolean ans;
-	int tNum = 8;
-	int lgTNum = 5;
+	int tNum = 32;
+	int len;
+	int hashCap;
 	Thread[] t = new Thread[tNum];
 
-	public boolean checkLLK(int[][] array) {
+	public boolean checkLLK(int[][] array) 
+	{
+		sort(array);	// merge sort: O(NlogN)
+		
 		ans = false;
-		int len = array.length;
-		int hashCap = 1 << ((int) Math.ceil(Math.log10((double) len) / 0.3010));
-
+		this.array = array;
+		len = array.length;
+		hashCap = 1 << ((int) Math.ceil(Math.log10((double) len) / 0.3010));
+		
 		// if array length is larger than the number of threads
-		if (len > tNum) {
-			for (int k = 0; k < tNum; k++) {
-				final int finalK = k;
-				t[k] = new Thread(new Runnable() {
-					public void run() {
-						final int start = (len * finalK) >> lgTNum;
-						final int end = (len * (finalK + 1)) >> lgTNum;
-						HashMap m = new HashMap(hashCap);
-
-						for (int i = start; i < end && !ans; i++) {
-							for (int j = i + 1; j < len && !ans; j++) {
-								if (m.containOrPut(
-										(double) (array[i][1] - array[j][1]) / (double) (array[i][0] - array[j][0]))) {
-									ans = true;
-								}
-							}
-							m.reset();
-						}
-					}
-				});
-				t[k].start();
+		if (len > tNum*2) 
+		{
+			MultiThread[] mt = new MultiThread[tNum];
+			
+			// split array to 8 pieces
+			for(int tr=0; tr<tNum; tr++) 
+			{
+				mt[tr] = new MultiThread(tr);
+				mt[tr].start();
 			}
-
-			try {
-				for (Thread v : t)
-					v.join();
-			}
-			catch (InterruptedException e) {
-			}
+			
+			try
+			{
+	            for(int tr=0; tr<tNum; tr++) 
+	            {
+	                mt[tr].join();	// merge all thread and wait end
+	            }
+	        }
+			catch(InterruptedException e) {}
 
 			return ans;
 		} 
-		else {
-			int i, j;
+		else 
+		{
+			int deltaX, deltaY,gcd;
 			HashMap m = new HashMap(hashCap);
-			for (i = 0; i < len; i++) {
-				for (j = i + 1; j < len; j++) {
-					if (m.containOrPut((double) (array[i][1] - array[j][1]) / (double) (array[i][0] - array[j][0]))) {
-						return true;
-					}
-				}
-				m.reset();
-			}
+			
+	        for(int i=0; i<len; i++)
+	        {
+	            for(int j=i+1; j<len; j++)
+	            {
+	                deltaX = array[i][0] - array[j][0];
+	                deltaY = array[i][1] - array[j][1];
 
-			return false;
+	                gcd = gcd(deltaY, deltaX);
+
+	                
+	                deltaX = (deltaX / gcd);
+	                deltaY = (deltaY / gcd);
+	                if(deltaX < 0)	// we prefer that x is positive
+	                {
+	                    deltaX = ~deltaX + 1;
+	                    deltaY = ~deltaY + 1;
+	                }
+
+	                if(m.containOrPut(deltaX, deltaY))	// if it is already store in hashmap, return true.
+	                {
+	                    return true;
+	                }
+	            }
+	            m.reset();
+	        }
+
+	        return false;
 		}
-
 	}
+	
+	class MultiThread extends Thread 
+	{
+    	int tr;
+    	
+    	public MultiThread(int tr) 
+    	{
+    		this.tr = tr;
+    	}
+    	
+    	public void run()
+    	{
+    		int start = len * tr / tNum;
+			int end = len * (tr + 1) / tNum;
+			HashMap m = new HashMap(hashCap);
 
-	private class HashMap {
-		class Entry {
-			public double key;
-			public Entry next;
-		}
+			int deltaX, deltaY,gcd;
+			for (int i = start; i < end && !ans; i++) 
+			{
+//				System.out.println("point "+i);
+				for (int j = i + 1; j < len && !ans; j++) 
+				{
+					deltaX = array[i][0] - array[j][0];
+	                deltaY = array[i][1] - array[j][1];
 
-		private int cap;
-		private Entry[] list;
+	                // get fraction in lowest term of deltaX / deltaY 
+	                gcd = gcd(deltaY, deltaX);
+	                deltaX = (deltaX / gcd);
+	                deltaY = (deltaY / gcd);
+	                
+	                if(deltaX < 0)	// we prefer that x is positive
+	                {
+	                    deltaX = ~deltaX + 1;
+	                    deltaY = ~deltaY + 1;
+	                }
 
-		HashMap(int size) {
-			this.cap = size;
-			Entry[] newEntry = new Entry[this.cap];
-			this.list = newEntry;
-		}
-
-		final public void reset() {
-			Entry[] newEntry = new Entry[this.cap];
-			this.list = newEntry;
-		}
-
-		final public boolean containOrPut(double key) {
-			// & (this.cap - 1) is equivalent to % this.cap when this.cap is power of 2
-			int index = (Double.valueOf(key).hashCode() & 0x7fffffff) & (this.cap - 1);
-
-			for (Entry current = list[index]; current != null; current = current.next) {
-				if (current.key == key) {
-					return true;	// if the value already contained in hashmap
+	                if(m.containOrPut(deltaX, deltaY))	// if it is already store in hashmap, return true.
+	                {
+	                    ans = true;
+	                }
 				}
+				m.reset();	// reset hashmap when one point is end;
 			}
 			
-			// if not, than add new entry at the head of hashmap
-			Entry newEntry = new Entry();
-			newEntry.key = key;
-			newEntry.next = list[index];
-			list[index] = newEntry;
-			return false;
+//			System.out.println("thread "+tr+" end!");
+    	}
+    }
+
+	private class HashMap
+    {
+        class Entry
+        {
+            public int key1;
+            public int key2;
+            public Entry next;
+        }
+
+        private int cap;
+        private Entry[] list;
+
+        HashMap(int size)
+        {
+            this.cap = size;
+            Entry[] newEntry = new Entry[this.cap];
+            this.list = newEntry;
+        }
+
+        final public void reset()
+        {
+            Entry[] newEntry = new Entry[this.cap];
+            this.list = newEntry;
+        }
+
+        final public boolean containOrPut(int key1, int key2)
+        {
+            // & (this.cap - 1) is equivalent to % this.cap when base of this.cap is 2
+            int index = ((key1 + key2) & 0x7fffffff) & (this.cap -1);
+            for(Entry current = list[index]; current!=null; current=current.next)
+            {
+                if(current.key1 == key1 && current.key2 == key2)
+                {
+                    return true;
+                }
+            }
+            Entry newEntry = new Entry();
+            newEntry.key1  = key1;
+            newEntry.key2  = key2;
+            newEntry.next = list[index];
+            list[index] = newEntry;
+            return false;
+        }
+    }
+	
+	public int gcd(int a, int b)
+    {
+        return b==0 ? a : gcd(b, a%b);
+    }
+	
+	public static void sort(int[][] a)
+	{
+		int[][] aux = new int[a.length][2];
+		sort(a, aux, 0, a.length-1);
+	}
+    
+    private static void sort(int[][] a, int[][] aux, int lo, int hi)
+	{
+		if(hi <= lo) return;
+		int mid = lo + (hi-lo)/2;
+		sort(a, aux, lo, mid);
+		sort(a, aux, mid+1, hi);
+		merge(a, aux, lo, mid, hi);
+	}
+    
+    private static void merge(int[][] a, int[][] aux, int lo, int mid, int hi)
+	{
+		for(int k = lo; k <= hi; k++) aux[k] = a[k];
+		
+		int i = lo, j = mid+1;
+		for(int k = lo; k <= hi; k++)
+		{
+			if(i > mid) a[k] = aux[j++];
+			else if(j > hi) a[k] = aux[i++];
+			else if(less(aux[i], aux[j])) a[k] = aux[i++];
+			else a[k] = aux[j++];
 		}
+	}
+    
+    private static boolean less(int[] i, int[] j)
+	{
+		if(i[0] < j[0]) return true;
+		else if(i[0] > j[0]) return false;
+		else
+			if(i[1] < j[1]) return true;
+			else if(i[1] > j[1]) return false;
+			else return true;
 	}
 }
--- src/hw05/HW05_4108056005_3.java
+++ src/hw05/HW05_4108056005_3.java
@@ -14,40 +14,35 @@ public class HW05_4108056005_3 {
 //		System.out.println("elapsed time " + time);
 	}
 
-    public boolean checkLLK(int[][] array){
-        int i,j;
+    public boolean checkLLK(int[][] array)
+    {
         int len = array.length;
         int lnLen = (int)Math.ceil(Math.log10((double)len) / 0.3010);
         HashMap m = new HashMap(1 << lnLen);
 
+        sort(array);
+        
         int deltaX, deltaY,gcd;
-        for(i=0; i<len; i++){
-            for(j=i+1; j<len; j++){
+        for(int i=0; i<len; i++)
+        {
+//        	System.out.println("point "+i);
+            for(int j=i+1; j<len; j++)
+            {
                 deltaX = array[i][0] - array[j][0];
                 deltaY = array[i][1] - array[j][1];
 
-                //If there is duplicate test data return true
-                //if(deltaX == 0 && deltaY == 0) return true;
-
                 gcd = gcd(deltaY, deltaX);
-
-                //If Both deltaX and deltaY are positive or negative
-                //Than deltaX/gcd and deltaY are positive
-
-                //If one of deltaX and deltaY is positive and the other is negative
-                //Than one of deltaX/gcd and deltaY/gcd is positive and the other is negative
-                //However, we don't know which one is positive or which one is negative
-                //Thus, we will keep deltaX is positive and deltaY is negative
-                //By changing their signed.
 
                 deltaX = (deltaX / gcd);
                 deltaY = (deltaY / gcd);
-                if(deltaX < 0){
+                if(deltaX < 0)	// we prefer that x is positive
+                {
                     deltaX = ~deltaX + 1;
                     deltaY = ~deltaY + 1;
                 }
 
-                if(m.containOrPut(deltaX, deltaY)){
+                if(m.containOrPut(deltaX, deltaY))	// if it is already store in hashmap, return true.
+                {
                     return true;
                 }
             }
@@ -57,8 +52,10 @@ public class HW05_4108056005_3 {
         return false;
     }
 
-    private class HashMap{
-        class Entry{
+    private class HashMap
+    {
+        class Entry
+        {
             public int key1;
             public int key2;
             public Entry next;
@@ -67,22 +64,27 @@ public class HW05_4108056005_3 {
         private int cap;
         private Entry[] list;
 
-        HashMap(int size){
+        HashMap(int size)
+        {
             this.cap = size;
             Entry[] newEntry = new Entry[this.cap];
             this.list = newEntry;
         }
 
-        final public void reset(){
+        final public void reset()
+        {
             Entry[] newEntry = new Entry[this.cap];
             this.list = newEntry;
         }
 
-        final public boolean containOrPut(int key1, int key2){
+        final public boolean containOrPut(int key1, int key2)
+        {
             // & (this.cap - 1) is equivalent to % this.cap when base of this.cap is 2
             int index = ((key1 + key2) & 0x7fffffff) & (this.cap -1);
-            for(Entry current = list[index]; current!=null; current=current.next){
-                if(current.key1 == key1 && current.key2 == key2){
+            for(Entry current = list[index]; current!=null; current=current.next)
+            {
+                if(current.key1 == key1 && current.key2 == key2)
+                {
                     return true;
                 }
             }
@@ -95,7 +97,47 @@ public class HW05_4108056005_3 {
         }
     }
     
-    public int gcd(int a, int b){
+    public int gcd(int a, int b)
+    {
         return b==0 ? a : gcd(b, a%b);
     }
+    
+    public static void sort(int[][] a)
+	{
+		int[][] aux = new int[a.length][2];
+		sort(a, aux, 0, a.length-1);
+	}
+    
+    private static void sort(int[][] a, int[][] aux, int lo, int hi)
+	{
+		if(hi <= lo) return;
+		int mid = lo + (hi-lo)/2;
+		sort(a, aux, lo, mid);
+		sort(a, aux, mid+1, hi);
+		merge(a, aux, lo, mid, hi);
+	}
+    
+    private static void merge(int[][] a, int[][] aux, int lo, int mid, int hi)
+	{
+		for(int k = lo; k <= hi; k++) aux[k] = a[k];
+		
+		int i = lo, j = mid+1;
+		for(int k = lo; k <= hi; k++)
+		{
+			if(i > mid) a[k] = aux[j++];
+			else if(j > hi) a[k] = aux[i++];
+			else if(less(aux[i], aux[j])) a[k] = aux[i++];
+			else a[k] = aux[j++];
+		}
+	}
+    
+    private static boolean less(int[] i, int[] j)
+	{
+		if(i[0] < j[0]) return true;
+		else if(i[0] > j[0]) return false;
+		else
+			if(i[1] < j[1]) return true;
+			else if(i[1] > j[1]) return false;
+			else return true;
+	}
 }
